eaf03416031837931d7fc82431f66f68856e1621
saasyoi <sulthanaidhanssn@gmail.com>
2026-08-12 10:52:37 +0700

    fix: change db host to 127.0.0.1 for CI TCP connection

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function pdo_connect(){
-    $DATABASE_HOST = 'localhost';
+    $DATABASE_HOST = '127.0.0.1';
     $DATABASE_USER = 'root';
     $DATABASE_PASS = '';
     $DATABASE_NAME = 'damncrud';
