407d4b9dbc8fff79e728afc3672870cc634aaff4
saasyoi <sulthanaidhanssn@gmail.com>
2026-08-12 10:35:43 +0700

    fix: correct password hash in db seed for ci

--- db/damncrud.sql
+++ db/damncrud.sql
@@ -55,7 +55,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 -- Dumping data for table damncrud.users: ~1 rows (approximately)
 INSERT INTO `users` (`id_user`, `username`, `password`) VALUES
-	(1, 'admin', '9feac9a05349e4ccc78fb9d9b4ab61b33f868c7f8b8acd56dc7303c1af0cb7ca');
+	(1, 'admin', 'cd7af172a7a9165ff9f8e74a78d7bbabcc4fe878c723d2b8894f6d74217c4a08');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
